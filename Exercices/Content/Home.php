f77b82df53347223ab5ec4b7bf763c20081be950
ole <duggo1@bfh.ch>
2014-10-27 19:29:10 +0100

    2

--- Exercices/Content/Home.php
+++ Exercices/Content/Home.php
@@ -1,12 +1,14 @@
 <?php
+
 function home() {
-	echo '
-     <h1>Willkommen auf mywgzimmer.ch!</h1>
+    ?>
+    <h1>Willkommen auf mywgzimmer.ch!</h1>
     <ul>
         <li>Bist du auf der Suche nach einem freien Zimmer?
             Dann klicke auf "WG-Zimmer suchen"!</li>
         <li>Hast du ein Zimmer zu vermieten und möchtest <strong>gratis</strong> ein Inserat aufgeben?
             Klicke auf "Gratis WG-Zimmer inserieren"!</li>
-    </ul>';
+    </ul>
+    <?php
 }
 ?>
